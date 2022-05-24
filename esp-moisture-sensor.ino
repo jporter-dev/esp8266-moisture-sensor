f19af9f5f5da0086ede8b8328c22133a7ab4a551
Josh Porter <j@prtr.dev>
2022-05-23 23:40:07 -0400

    feat: add wifi

--- esp-moisture-sensor.ino
+++ esp-moisture-sensor.ino
@@ -1,23 +1,63 @@
+#include "ESP8266WiFi.h"
+
 #define SensorPin A0
 
+// wifi
+const char *ssid = "";
+const char *password = "";
 
+// mqtt
+const char* mqtt_server = "1.1.1.1";
+const char* sensor_topic = "moisture";
+const char* mqtt_username = "username";
+const char* mqtt_password = "password";
+
+// device identifier
+const char* clientID = "test_esp_moisture_sensor";
+
+WiFiClient wifiClient;
 
 void setup() {
-  delay(10000);
   Serial.begin(115200);
-  Serial.println("Start");
-  Serial.println("---------------");
   delay(1000);
-  Serial.println("Blink Example");
+
+  // setup wifi
+  connectToWiFi();
+
+  // setup builtin led
   delay(1000);
   pinMode(LED_BUILTIN, OUTPUT);
 
+  // setup sensor pin
   Serial.setTimeout(2000);
   float sensorValue = analogRead(SensorPin);
   Serial.println(sensorValue);
-
 }
 
+void connectToWiFi() {
+//Connect to WiFi Network
+  Serial.println();
+  Serial.println();
+  Serial.print("Connecting to Wifi");
+  Serial.println("...");
+  WiFi.begin(ssid, password);
+  int retries = 0;
+
+  while ((WiFi.status() != WL_CONNECTED) && (retries < 15)) {
+    retries++;
+    delay(500);
+    Serial.print(".");
+  }
+  if (retries > 14) {
+      Serial.println(F("Wifi connection FAILED!"));
+  }
+  if (WiFi.status() == WL_CONNECTED) {
+      Serial.println(F("Wifi connected!"));
+      Serial.println("IP address: ");
+      Serial.println(WiFi.localIP());
+  }
+    Serial.println(F("Setup ready"));
+}
 
 void loop() {
 Serial.println("========================");
@@ -30,5 +70,5 @@ Serial.println("Loop");
   Serial.println(sensorValue);
 
   digitalWrite(LED_BUILTIN, HIGH);
-  delay(2000);
+  delay(5000);
 }
